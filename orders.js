59da01ed0c344c935479e7c7b7e8ab3944618937
abdorrady <114504097+abdorrady@users.noreply.github.com>
2022-10-11 15:52:49 +0200

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders  = [
+    {
+        productName:'Foldable Mini Drone',
+        productNumber:'8543',
+        paymentStatues:'Due',
+        shipping:'Pending'
+
+    },
+    {
+        productName:'Foldable Mini Drone',
+        productNumber:'7812',
+        paymentStatues:'Due',
+        shipping:'Pending'
+
+    },
+    {
+        productName:'Foldable Mini Drone',
+        productNumber:'8543',
+        paymentStatues:'Due',
+        shipping:'Pending'
+
+    },
+    {
+        productName:'Foldable Mini Drone',
+        productNumber:'1900',
+        paymentStatues:'Due',
+        shipping:'Pending'
+
+    },
+    {
+        productName:'Foldable Mini Drone',
+        productNumber:'8543',
+        paymentStatues:'Due',
+        shipping:'Pending'
+
+    },
+    {
+        productName:'Foldable Mini Drone',
+        productNumber:'3485',
+        paymentStatues:'Due',
+        shipping:'Pending'
+
+    }
+]
